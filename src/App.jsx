ecac44b610bed386074d365914087935248bc618
Andreas Avetisian <t0avan00@students.oamk.fi>
2024-04-10 01:45:38 +0200

    Navbar and "Hero" section implementation
    Added "assets" folder with prepared icons and images;
    Implemented Navbar and "Hero" section using Tailwind CSS;

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
-export default function App() {
+import  {
+  Hero,
+  CustomerReviews,
+  Footer,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  PopularProducts
+} from './sections';
+import Nav from './components/Nav';
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="relative">
+      <Nav />
+      <section className="xl:pad-l wide:pad-r pad-b">
+        <Hero />
+      </section>
+      <section className="pad">
+        <PopularProducts />
+      </section>
+      <section className="pad">
+        <SuperQuality />
+      </section>
+      <section className="pad-x py-10">
+        <Services />
+      </section>
+      <section className="pad">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue pad">
+        <CustomerReviews />
+      </section>
+      <section className="pad-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black pad-x pad-t pb-8">
+        <Footer />
+      </section>
+    </main>
   )
 }
+
+export default App
